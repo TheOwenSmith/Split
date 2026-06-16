2ea8657098f0dcd1438fa2a786ed4757a172e694
Owen Smith <owensmith@uchicago.edu>
2026-06-16 09:26:38 -0500

    Initial Commit

---
+++ split/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  split
+//
+//  Created by Lauren Jurgens on 6/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ split/splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitApp.swift
+//  split
+//
+//  Created by Lauren Jurgens on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
